9ca723ffd81f3011e338592c30035088db44d8ff
falivith <falivith@gmail.com>
2023-08-28 22:15:53 -0300

    Register Screen

--- where-front/src/main.tsx
+++ where-front/src/main.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from './pages/login.tsx'
+import Login from './pages/login.tsx';
+import Register from './pages/Register.tsx';
 
 const router = createBrowserRouter([
   {
-  path: "/",
-  element: <Login/>
+    path: "/",
+    element: <Login/>
   },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "./map",
+    element: <Map/>
+  }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
