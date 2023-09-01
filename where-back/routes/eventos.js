fea2b639ef7611ad23739e36f1e1573d5b622aa7
Afonso <afonsotc@gmail.com>
2023-09-01 11:38:23 -0300

    Create/validate event

--- where-back/routes/eventos.js
+++ where-back/routes/eventos.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const { participam, locais,eventos, promoters} = require('../models');
 const {registerValidation} = require('../utils/validation')
 const {validateToken} = require('../utils/JWT')
+const {eventValidation} = require('../utils/validation')
 
 
 router.get('/', validateToken, async function(req,res,next) {
    try {
       // get all event from database
-      const listEventos = await eventos.findAll();
+      const listEventos = await eventos.findAll({ attributes : {exclude: ['email_fk']}});
       return res.status(200).json(listEventos);
    } catch(error){
       return res.status(400).json(error);
@@ -36,6 +37,9 @@ router.post('/create', validateToken, async function(req,res,next){
           {email_fk: req.username} });
    if(!isPromoter) return res.status(400).json({isPromoter: false});
 
+   //Verify input
+    const {error} = eventValidation(req.body);
+   if (error) return res.status(400).json({isPromoter:true, isValidated: false, error:error.details[0].message})
 
    try {
          await eventos.create({
@@ -48,20 +52,37 @@ router.post('/create', validateToken, async function(req,res,next){
          codEvento_fk: req.body.codEvento_fk,
          latitude_fk: req.body.latitude_fk,
          longitude_fk: req.body.longitude_fk,
-         horario: req.body.horario
+         horario: req.body.horario,
+         estabelecimento: req.body.estabelecimento
       }).then(
           evento => {return res
               .status(200)
               .json({
                   isPromoter: true,
+                  isValidated: true,
                   isCreated: true,
                   id:evento.codEvento})});
    } catch (error){
-      return res.status(400).json({isPromoter: true, isCreated:false, error});
+      return res.status(400).json({isPromoter: true, isValidated:true, isCreated:false, error});
    }
 })
 
 router.get('/:id', validateToken, async  function(req, res, next) {
 
+    //Get event
+    const event = await eventos.findByPk(req.params.id);
+
+    // Verify if event exists
+    if(!event) return res.status(400).json({isEvent:false});
+
+    event.email_fk = undefined;
+    return res.status(200).json({isEvent:true, event:event});
 })
+
+router.get('/interested', validateToken, async function(req, res, next) {
+
+
+})
+
+
 module.exports = router;
--- where-back/utils/validation.js
+++ where-back/utils/validation.js
@@ -39,26 +39,44 @@ const loginValidation = data => {
 }
 
 //Event validation
-// const registerValidation = data => {
-//     const schema = Joi.object({
-//         name: Joi.string()
-//             .min(6)
-//             .max(60)
-//             .required(),
-//         email: Joi.string()
-//             .min(6)
-//             .max(60)
-//             .required()
-//             .email(),
-//         password: Joi.string()
-//             .min(8)
-//             .max(32)
-//             .required(),
-//         date: Joi.date().format('YYYY-MM-DD')
-//             .required(),
-//     });
-//     return schema.validate(data);
-// }
+const eventValidation = data => {
+    const schema = Joi.object({
+        codEvento_fk: Joi.number()
+            .integer()
+            .optional()
+            .min(0)
+            .allow(null),
+        nome: Joi.string()
+            .min(6)
+            .max(100)
+            .required(),
+        endereco: Joi.string()
+            .min(6)
+            .max(100)
+            .required(),
+        estabelecimento: Joi.string()
+            .min(3)
+            .max(100)
+            .required(),
+        inicio: Joi.date().format('YYYY-MM-DD')
+            .required(),
+        fim: Joi.date().format('YYYY-MM-DD'),
+        horario: Joi.date().format('YYYY-MM-DD HH:mm:ss')
+            .required(),
+        descricao: Joi.string()
+            .min(0)
+            .max(65535),
+        latitude: Joi.string().regex(/^(\+|-)?(?:90(?:(?:\.0{1,8})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,8})?))$/, 'numbers').max(12).min(12)
+            .min(8)
+            .max(32),
+        longitude: Joi.string().regex(/^(\+|-)?(?:180(?:(?:\.0{1,8})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,8})?))$/, 'numbers').max(12).min(12)
+            .min(8)
+            .max(32),
+    });
+    return schema.validate(data);
+}
 
 module.exports.registerValidation = registerValidation;
-module.exports.loginValidation = loginValidation
+module.exports.loginValidation = loginValidation;
+module.exports.eventValidation = eventValidation;
+;
